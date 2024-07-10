34c9a5c320fc39d66db1b5e18695467fdfb4b04e
sorspetar111 <156424190+sorspetar111@users.noreply.github.com>
2024-07-10 20:01:25 +0300

    Add files via upload

---
+++ TT.Api/Controllers/DataController.cs
@@ -0,0 +1,146 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TT.Lib.DataAccess;
+using TT.Lib.Entities;
+using TT.Lib.Mvc;
+
+namespace TT.Api.Controllers
+{
+   
+
+    public class DataFactoryController<TEntity, TService> : ReadWriteController<TEntity, int>
+        where TEntity : class, IId, new()
+        where TService : IService<TEntity>
+    {
+        private readonly TService service;
+
+        public DataFactoryController(TService service) : base((IReadWriteService<TEntity>)service)
+        {
+            this.service = service;
+        }
+
+        [HttpGet("all")]
+        public async Task<IActionResult> GetAll()
+        {
+            var entities = await service.GetAll();
+            return Ok(entities);
+        }
+    }
+
+
+
+
+
+    [Route("[controller]")]
+    public class DataProductController : DataFactoryController<Product, IProductService>
+    {
+        private readonly IProductService productService;
+
+        public DataProductController(IProductService productService) : base(productService)
+        {
+            this.productService = productService;
+        }
+
+        [HttpGet("products")]
+        public async Task<IActionResult> GetAllProducts()
+        {
+            var products = await productService.GetAllProducts_SP();
+            return Ok(products);
+        }
+    }
+
+    [Route("[controller]")]
+    public class DataPropertiesController : DataFactoryController<Property, IPropertyService>
+    {
+        private readonly IPropertyService propertyService;
+
+        public DataPropertiesController(IPropertyService propertyService) : base(propertyService)
+        {
+            this.propertyService = propertyService;
+        }
+
+        [HttpGet("properties")]
+        public async Task<IActionResult> GetAllProperties()
+        {            
+            var properties = await this.propertyService.GetAll();
+            return Ok(properties);
+        }
+    }
+
+
+
+
+
+    /*
+   public class DataBaseController : ReadWriteController<Product, int>
+   {
+       private readonly IProductService productService;
+       private readonly IPropertyService propertyService;
+
+       public DataBaseController(IProductService productService, IPropertyService propertyService) : base((IReadWriteService<Product>)productService)
+       {
+           this.productService = productService;
+           this.propertyService = propertyService;
+       }
+
+
+   }
+
+   [Route("[controller]")]
+   public class DataProductController : DataBaseController
+   {
+       private readonly IProductService productService;
+
+
+       public DataProductController(IProductService productService) : base((IReadWriteService<Product>)productService)
+       {
+           this.productService = productService;
+
+       }
+
+       [HttpGet("products")]
+       public async Task<IActionResult> GetAllProducts()
+       {
+           var products = await this.productService.GetAllProducts_SP();
+           return Ok(products);
+       }
+
+
+   }
+
+
+   [Route("[controller]")]
+   public class DataPropertiesController : DataBaseController
+   {
+
+       private readonly IPropertyService propertyService;
+
+       public DataPropertiesController(IProductService productService) : base((IReadWriteService<Property>)propertyService)
+       {
+           this.propertyService = propertyService;
+       }
+
+
+       [HttpGet("properties")]
+       public async Task<IActionResult> GetAllProperties()
+       {
+           new NotImplementedException("Task 2 is not implement.");
+           return null;
+
+           //var properties = await this.propertyService.GetAllProperies_View();
+           // return Ok(properties);
+       }
+   }
+
+
+   */
+
+
+
+
+
+
+
+}
---
+++ TT.Api/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TT.Lib.DataAccess;
+using TT.Lib.Entities;
+using TT.Lib.Mvc;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace TT.Api.Controllers
+{
+    [Route("[controller]")]
+    public class ProductController :  ReadWriteController<Product, int>
+    {
+        private readonly IProductService mainService;
+
+        public ProductController(IProductService mainService) : base((IReadWriteService<Product>)mainService)
+        {
+            this.mainService = mainService;
+        }
+
+        [HttpDelete]
+        public new async Task<IActionResult> Delete(Product product) => Ok(await base.Delete(product));
+
+        [HttpGet]
+        [Route("[action]/{id}")]
+        public async Task<IActionResult> GetWithObjects(int id) {
+            var p = (await this.mainService.Query())
+                .Where(x => x.Id == id)
+                .Include(x => x.Brand)
+                .FirstOrDefault();
+
+            return Ok(p);
+        }
+    }
+
+
+}
